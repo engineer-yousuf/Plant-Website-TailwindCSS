f764051fd69957906b009c39dda3e30b1b691e80
engineersohag <engineer.sohag.cse@gmail.com>
2025-01-25 16:15:55 +0600

    Service Section Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,22 @@ module.exports = {
     extend: {},
     keyframes: {
       move: {
-        "50%": { transform: "tranlateY(-1rem)" }
+        "50%": { transform: "translateY(-1rem)" }
+      },
+      rotate: {
+        "0%": { transform: "rotate(0deg)" },
+        "100%": { transform: "rotate(360deg)" },
+      },
+      scaleUp: {
+        "0%": { transform: "scale(0.8)" },
+        "50%": { transform: "scale(1.2)" },
+        "100%": { transform: "scale(0.8)" },
       }
     },
     animation: {
-      movingY: "move 3s linear infinite"
+      movingY: "move 3s linear infinite",
+      rotating: "rotate 15s linear infinite",
+      scalingUp: "scaleUp 3s linear infinite",
     },
     fontFamily: {
       Jost: ["Jost", "serif"],
